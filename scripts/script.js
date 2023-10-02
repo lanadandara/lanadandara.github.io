ed894444d9e74a187985cdb225333fdccbb41ef5
Lana Dandara <85411489+lanadandara@users.noreply.github.com>
2023-10-01 21:47:28 -0300

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,37 @@
+let btn = document.querySelector(".nav-button");
+        let ul = document.querySelector("ul");
+
+        btn.onclick = function() {
+            ul.classList.toggle("show");
+        };
+
+let storeManagement = document.getElementById("card-store-management");
+let digitalWallet = document.getElementById("card-digital-wallet");
+let storeRestock = document.getElementById("card-store-restock");
+
+storeManagement.addEventListener("click", () => {
+  window.location.href = "/store-management.html"; 
+});
+
+digitalWallet.addEventListener("click", () => {
+    window.location.href = "/digital-wallet.html"; 
+  });
+
+storeRestock.addEventListener("click", () => {
+    window.location.href = "/store-restock.html"; 
+  });
+
+
+
+let scrollButton = document.getElementById('contact-button');
+
+  scrollButton.addEventListener('click', () => {
+    scrollToLastDiv();
+  });
+  
+  function scrollToLastDiv() {
+    const lastDiv = document.getElementById('footer-contact');
+    lastDiv.scrollIntoView({ behavior: 'smooth' });
+  };
+
+
