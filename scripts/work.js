e2881d0149f47c3fd5817277a3102bcf604a6662
Lana Dandara <85411489+lanadandara@users.noreply.github.com>
2025-01-30 10:12:14 -0300

    Add files via upload

--- scripts/work.js
+++ scripts/work.js
@@ -24,3 +24,8 @@ let scrollButton = document.getElementById('contact-button');
   scrollButton.addEventListener('click', () => {
     scrollToLastDiv();
   });
+  
+  function scrollToLastDiv() {
+    const lastDiv = document.getElementById('footer-contact');
+    lastDiv.scrollIntoView({ behavior: 'smooth' });
+  };
